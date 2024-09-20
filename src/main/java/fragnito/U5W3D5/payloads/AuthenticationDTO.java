9b3c5626f633d1070b1ab560fdf473237e65912c
C043 <mariofragnitoph@gmail.com>
2024-09-20 14:24:19 +0200

    Adds NewUserDTO

--- src/main/java/fragnito/U5W3D5/payloads/AuthenticationDTO.java
+++ src/main/java/fragnito/U5W3D5/payloads/AuthenticationDTO.java
@@ -9,7 +9,7 @@ public record AuthenticationDTO(
         @Email(message = "L'email inserita non è un indirizzo valido")
         String email,
         @NotNull(message = "La password è obbligatoria")
-        @Size(min = 10, max = 20, message = "La password deve essere di minimo 10 e massimo 20 caratteri")
+        @Size(min = 8, max = 20, message = "La password deve essere di minimo 8 e massimo 20 caratteri")
         String password
 ) {
 }
