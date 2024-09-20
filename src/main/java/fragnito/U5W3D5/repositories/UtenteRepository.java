96903345a798240f3af916e4abc0bc4590c70f58
C043 <mariofragnitoph@gmail.com>
2024-09-20 11:56:39 +0200

    Adds Utente Repository

---
+++ src/main/java/fragnito/U5W3D5/repositories/UtenteRepository.java
@@ -0,0 +1,9 @@
+package fragnito.U5W3D5.repositories;
+
+import fragnito.U5W3D5.entities.Utente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UtenteRepository extends JpaRepository<Utente, Integer> {
+}
